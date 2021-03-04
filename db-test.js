6cfdacdaccb746f8485c33fbf2d2844e55acc2be
karnurmax <karnurmax@mail.ru>
2021-03-05 00:30:56 +0500

    use native mysql instead of SequelizeORM

--- db-test.js
+++ db-test.js
@@ -1,24 +1,32 @@
-const { Sequelize } = require('sequelize');
-const conf = require('./env/config');
-const SequelizeAuto = require('sequelize-auto');
-
-// const sequelize = new Sequelize('sqlite::memory:');
-// const sequelize = new Sequelize('mysql://root:qweQWE123@localhost:3306/p-317273_taxi_db');
+const { db } = require('./env/config');
+var mysql = require('mysql');
 
 
-const sequelize = new Sequelize(conf.db.name, conf.db.user, conf.db.password, {
-    host: conf.db.host,
-    dialect: conf.db.dialect
-});
 
 module.exports = () => {
     return new Promise(async(resolve, reject) => {
-        try {
-            await sequelize.authenticate();
+        var connection = mysql.createConnection({
+            host: db.host,
+            user: db.user,
+            password: db.password,
+            database: db.name
+        });
+        connection.connect();
+
+        connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
+            if (error) {
+                reject({ msg: 'Unable to connect to the database:', error });
+                connection.end();
+                return;
+            }
             resolve('Connection has been established successfully')
-        } catch (error) {
-            reject({ msg: 'Unable to connect to the database:', error });
-        }
+        });
+
+        connection.end();
+
+
+
+
     });
 
 }
