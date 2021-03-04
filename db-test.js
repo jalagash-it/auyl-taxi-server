25e909fbc5195939934c12196c36932b218735d7
karnurmax <karnurmax@mail.ru>
2021-03-04 22:13:36 +0500

    test db connection

--- db-test.js
+++ db-test.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize');
 const conf = require('./env/config');
 
-const sequelize = new Sequelize(conf.db.name, conf.db.user, conf.db.password, {
-    host: conf.db.host,
-    dialect: conf.db.dialect
-});
+const sequelize = new Sequelize(`mysql://nurlan:qweQWE123@localhost:3306/p-317273_taxi_db`);
+// const sequelize = new Sequelize(conf.db.name, conf.db.user, conf.db.password, {
+//     host: conf.db.host,
+//     dialect: conf.db.dialect
+// });
 
 module.exports = () => {
     return new Promise(async(resolve, reject) => {
