484f1d06a6b57fd3a600172dc72876dc54000e46
karnurmax <karnurmax@mail.ru>
2021-03-05 01:00:33 +0500

    add mariadb tests

--- db-test.js
+++ db-test.js
@@ -1,9 +1,49 @@
 const { db } = require('./env/config');
 var mysql = require('mysql');
 
+const mariadb = require('mariadb');
 
 
 module.exports = () => {
+    return testMariadb();
+
+}
+const testMariadb = () => {
+    return new Promise(async(resolve, reject) => {
+        const pool = mariadb.createPool({
+            database: db.name,
+            host: db.host,
+            port: db.port,
+            user: db.user,
+            password: db.password,
+            connectionLimit: 5
+        });
+        pool.getConnection()
+            .then(conn => {
+
+                conn.query("SELECT 1 as val")
+                    .then((rows) => {
+                        console.log(rows); //[ {val: 1}, meta: ... ]
+                        //Table must have been created before 
+                        // " CREATE TABLE myTable (id int, val varchar(255)) "
+                        return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+                    })
+                    .then((res) => {
+                        console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+                        conn.end();
+                    })
+                    .catch(err => {
+                        //handle error
+                        console.log(err);
+                        conn.end();
+                    })
+
+            }).catch(err => {
+                //not connected
+            });
+    });
+}
+const testMysql = () => {
     return new Promise(async(resolve, reject) => {
         var connection = mysql.createConnection({
             host: db.host,
@@ -24,9 +64,5 @@ module.exports = () => {
 
         connection.end();
 
-
-
-
     });
-
 }
