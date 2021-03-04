b23fea52678e51b8057da4043d6fdfdc3760e296
karnurmax <karnurmax@mail.ru>
2021-03-04 23:42:54 +0500

    set older version of sequelize

--- db-test.js
+++ db-test.js
@@ -1,11 +1,15 @@
 const { Sequelize } = require('sequelize');
 const conf = require('./env/config');
+const SequelizeAuto = require('sequelize-auto');
 
-const sequelize = new Sequelize(`mysql://nurlan:qweQWE123@localhost:3306/p-317273_taxi_db`);
-// const sequelize = new Sequelize(conf.db.name, conf.db.user, conf.db.password, {
-//     host: conf.db.host,
-//     dialect: conf.db.dialect
-// });
+// const sequelize = new Sequelize('sqlite::memory:');
+// const sequelize = new Sequelize('mysql://root:qweQWE123@localhost:3306/p-317273_taxi_db');
+
+
+const sequelize = new Sequelize(conf.db.name, conf.db.user, conf.db.password, {
+    host: conf.db.host,
+    dialect: conf.db.dialect
+});
 
 module.exports = () => {
     return new Promise(async(resolve, reject) => {
